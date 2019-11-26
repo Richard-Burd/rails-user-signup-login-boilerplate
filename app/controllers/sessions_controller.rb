3e38de697784707f172ae70859a978220a1c0fce
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-11-26 10:14:54 -0800

    handles unknown github email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Check out this video here: https://www.youtube.com/watch?v=UAvuo-EbTFY (14:42 / 56:18)
+    # ...where Avi starts debugging this
+
     # Here is where the code will login using Omniauth-GitHub
-    if auth_hash = request.env["omniauth.auth"]
+    if auth_hash = request.env["omniauth.auth"] #=> If this object exists, then the user signed in with OAuth
       user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = user.id
       # raise params.inspect #=> you can enter these queries into the Rails web console and obtain the relevant values:
       # auth_hash[:info][:name]...request.env[:omniauth.auth][:credentials][:token]...request.env[:omniauth.auth][:provider]
+      # ...if these don't show up in the Rails web console, something is broken
       redirect_to root_path
     else # If there's no auth_hash, the user isn't using OmniAuth, so at this point the code will sign them in with their email & password
       # raise params.inspect #=> you can enter these queries into the Rails web console and obtain the relevant values:
-      # params[:email]...params[:password]...params[:authenticity_token]...if these don't show up, something is broken
+      # params[:email]...params[:password]...params[:authenticity_token]...if these don't show up in the Rails web console, something is broken
       user = User.find_by(:email => params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,23 @@ class User < ApplicationRecord
 
   def self.find_or_create_by_omniauth(auth_hash)
     # I changed the ["info"]["email"] to ["info"]["name"] because the email value is
-    # "nill" when I type "auth_hash["info"]["email"]" into the browser's error console
-    self.where(:email => auth_hash["info"]["name"]).first_or_create do |user|
+    # "nill" when I type "auth_hash["info"]["email"]" into the browser's error console.
+    # this is because in my particular
+    self.where(:username => auth_hash["info"]["name"]).first_or_create do |user|
       # for simplification purposes, the username & email for an omniauth user
       # will be exactly the same value because we never need to know the email
       # address of the user in this program, additionally, there is no actual
       # email address exposed within the auth_hash from GitHub.
-      user.password = SecureRandom.hex, user.username = auth_hash["info"]["name"]
+      user.password = SecureRandom.hex,
+      if auth_hash["info"]["email"] == nil # that is, no email address is listed.
+        # some GitHub users have their email set to private.  If that is the
+        # case, the user's email address will be set to this value below:
+        user.email = "unknown: GitHub doesn't share this user's email"
+      else # if the user's GitHub account has a public email address...
+        # the user's email address in this app will be the same as their GitHub
+        # email address.
+        user.email = auth_hash["info"]["email"]
+      end
     end
   end
 end
